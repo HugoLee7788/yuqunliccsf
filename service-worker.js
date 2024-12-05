a5b50bbc55bf99f021e70c871e025a93b692fa52
HugoLee7788 <141663008+HugoLee7788@users.noreply.github.com>
2024-12-04 19:03:03 -0800

    Add files via upload
    Signed-off-by: HugoLee7788 <141663008+HugoLee7788@users.noreply.github.com>

---
+++ service-worker.js
@@ -0,0 +1,15 @@
+// service-worker.js
+const CACHE_NAME = 'hybrid-solutions-v1';
+const urlsToCache = ['/', '/styles.css'];
+
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache))
+    );
+});
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request).then((response) => response || fetch(event.request))
+    );
+});
